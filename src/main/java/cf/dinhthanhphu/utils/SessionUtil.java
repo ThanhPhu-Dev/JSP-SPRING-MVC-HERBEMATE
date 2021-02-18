3162c939b06ad1eea310f3f79d778322c5666292
ThanhPhu-Dev <tphu2000@gmail.com>
2021-02-18 17:51:34 +0700

    tạo sessionUtil

--- src/main/java/cf/dinhthanhphu/utils/SessionUtil.java
+++ src/main/java/cf/dinhthanhphu/utils/SessionUtil.java
@@ -1,14 +1,28 @@
 package cf.dinhthanhphu.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class SessionUtil {
 	
-	public void putValue() {
-		
+	private static SessionUtil sessionUtil = null;
+	
+	public static SessionUtil getInstance() {
+		if(sessionUtil == null)
+		{
+			sessionUtil = new SessionUtil();
+		}
+		return sessionUtil;
 	}
 	
-	public Object getValue() {
-		return null;
+	public void putValue(HttpServletRequest req, String key, Object value) {
+		req.getSession().setAttribute(key, value);
 	}
 	
+	public Object getValue(HttpServletRequest req, String key ) {
+		return req.getSession().getAttribute(key);
+	}
 	
+	public void removeValue(HttpServletRequest req, String key) {
+		req.getSession().removeAttribute(key);
+	}
 }
